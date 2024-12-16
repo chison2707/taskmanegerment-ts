a26ccc1ffd2595d3c956a0272b70aaef8e253d3e
chison2707 <chiphansonzz17@gmail.com>
2024-12-16 21:45:28 +0700

    bộ lọc theo trạng thái

--- api/v1/controllers/task.controller.ts
+++ api/v1/controllers/task.controller.ts
@@ -2,6 +2,7 @@ import { Request, Response } from 'express';
 import Task from "../models/task.model";
 
 export const index = async (req: Request, res: Response) => {
+    // find
     interface Find {
         deleted: boolean,
         status?: string
@@ -14,7 +15,14 @@ export const index = async (req: Request, res: Response) => {
         find.status = req.query.status.toString();
     }
 
-    const tasks = await Task.find(find);
+    // sort
+    const sort = {};
+    if (req.query.sortKey && req.query.sortValue) {
+        const sortKey = req.query.sortKey.toString();
+        sort[sortKey] = req.query.sortValue;
+    }
+
+    const tasks = await Task.find(find).sort(sort);
 
     res.json(tasks);
 }
