5db95bee6910fbdc11d54eb57f103a119024835a
chison2707 <chiphansonzz17@gmail.com>
2024-12-16 16:53:32 +0700

    lấy ra danh sách công việc

--- index.ts
+++ index.ts
@@ -1,1 +1,24 @@
-console.log("ok")
+import express, { Express, Request, Response } from "express";
+import dotenv from "dotenv";
+import * as database from "./config/database";
+import Task from "./models/task.model";
+
+dotenv.config();
+
+database.connect();  // Connect to MongoDB
+
+const app: Express = express()
+const port: Number | String = process.env.PORT || 3000;
+
+app.get('/tasks', async (req: Request, res: Response) => {
+    const tasks = await Task.find({
+        deleted: false
+    })
+
+    console.log(tasks)
+    res.json(tasks);
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
