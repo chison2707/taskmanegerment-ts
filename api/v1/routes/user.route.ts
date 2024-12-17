2bf7063a1842b73b0a63bacdb40aa57ada8fafc2
chison2707 <chiphansonzz17@gmail.com>
2024-12-17 14:42:25 +0700

    Deploy code1

--- api/v1/routes/user.route.ts
+++ api/v1/routes/user.route.ts
@@ -7,10 +7,6 @@ import { requireAuth } from "../middlewares/auth.middleware";
 
 router.post('/register', controller.register);
 router.post('/login', controller.login);
-// router.post('/password/forgot', controller.forgotPassword);
-// router.post('/password/otp', controller.otpPassword);
-// router.post('/password/reset', controller.resetPassword);
 router.get('/detail', requireAuth, controller.detail);
-// router.get('/list', authMiddleware.requireAuth, controller.list);
 
 export const userRouter: Router = router;
--- dist/api/v1/controllers/user.controller.js
+++ dist/api/v1/controllers/user.controller.js
@@ -15,9 +15,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.detail = exports.login = exports.register = void 0;
 const md5_1 = __importDefault(require("md5"));
 const user_model_1 = __importDefault(require("../models/user.model"));
-// const ForgotPassword = require("../models/forgot-password.model");
 const generate_1 = require("../../../helper/generate");
-// const sendMailHelper = require("../../../helper/sendMail");
 // [POST]/api/v1/users/register
 const register = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     const existEmail = yield user_model_1.default.findOne({
@@ -85,96 +83,3 @@ const detail = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     });
 });
 exports.detail = detail;
-// // [POST]/api/v1/users/password/forgot
-// module.exports.forgotPassword = async (req, res) => {
-//     const email = req.body.email;
-//     const user = await User.findOne({
-//         email: email,
-//         deleted: false
-//     });
-//     if (!user) {
-//         res.json({
-//             code: 400,
-//             message: "Email không tồn tại"
-//         });
-//         return;
-//     }
-//     const otp = generateRandomNumber(6);
-//     const objForgotPassword = {
-//         email: email,
-//         otp: otp,
-//         expireAt: Date.now()
-//     };
-//     const forgotPassword = new ForgotPassword(objForgotPassword);
-//     await forgotPassword.save();
-//     // gửi otp qua email user
-//     const subject = "Mã OTP để lấy lại mật khẩu";
-//     const html = `
-//     Mã OTP để lấy lại mật khẩu của bạn là <b>${otp}</b>.
-//     Vui lòng không chia sẻ mã OTP này với bất kỳ ai.
-//     `;
-//     sendMailHelper.sendMail(email, subject, html);
-//     res.json({
-//         code: 200,
-//         message: "Đã gửi mã OTP qua email"
-//     });
-// };
-// // [POST]/api/v1/users/password/otp
-// module.exports.otpPassword = async (req, res) => {
-//     const email = req.body.email;
-//     const otp = req.body.otp;
-//     const result = await ForgotPassword.findOne({
-//         email: email,
-//         otp: otp
-//     });
-//     if (!result) {
-//         res.json({
-//             code: 400,
-//             message: "Mã OTP không đúng"
-//         });
-//         return;
-//     }
-//     const user = await User.findOne({
-//         email: email
-//     });
-//     const token = user.token;
-//     res.cookie("token", token);
-//     res.json({
-//         code: 200,
-//         message: "Xác thực thành công!",
-//         token: token
-//     });
-// };
-// // [POST]/api/v1/users/password/reset
-// module.exports.resetPassword = async (req, res) => {
-//     const token = req.body.token;
-//     const password = req.body.password;
-//     const user = await User.findOne({
-//         token: token
-//     });
-//     if (md5(password) === user.password) {
-//         res.json({
-//             code: 400,
-//             message: "Mật khẩu đã tồn tại"
-//         });
-//         return;
-//     }
-//     await User.updateOne({
-//         token: token
-//     }, {
-//         password: md5(password)
-//     });
-//     res.json({
-//         code: 200,
-//         message: "Đặt lại mật khẩu thành công"
-//     });
-// };
-// // [GET]/api/v1/users/list
-// module.exports.list = async (req, res) => {
-//     const users = await User.find({ deleted: false }).select("fullName email");
-//     res.json({
-//         code: 200,
-//         message: "Thành công",
-//         users: users
-//     });
-// };
