3aa5a2aacfef07f1c382b1dbe8f30059952f0381
chison2707 <chiphansonzz17@gmail.com>
2024-12-17 13:18:29 +0700

    cài đặt cors

--- index.ts
+++ index.ts
@@ -2,6 +2,7 @@ import express, { Express } from "express";
 import dotenv from "dotenv";
 import * as database from "./config/database";
 import mainV1Routes from "./api/v1/routes/index.route";
+import cors from "cors";
 import bodyParser from "body-parser";
 
 dotenv.config();
@@ -10,6 +11,13 @@ database.connect();  // Connect to MongoDB
 
 const app: Express = express()
 const port: Number | String = process.env.PORT || 3000;
+
+// var corsOptions = {
+//     origin: 'http://example.com',
+//     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+// }
+// app.use(cors(corsOptions));
+app.use(cors());
 
 app.use(bodyParser.json());
 
